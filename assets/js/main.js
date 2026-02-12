051f110671fd31a8994f8b9b6748a42731907fa6
Alanperez-ga <alanperez.ga@gmail.com>
2026-02-11 21:46:31 -0300

    Se agregan nuevos datos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,29 @@
-// Declaración de variables
+let desarrollador1 = {
+		nombre: 'Juan', // Clave + Valor = Atributo
+		apellido: 'Adasme',
+		edad: 32,
+		ocupación: 'Desarrollador Senior',
+		empleo: {
+				sector: 'banca',
+				empresa: 'Scotiabank',
+				cargo: 'Backend Developer',
+				salario: 3000000
+		},
+		//Método: función dentro de un objeto
+		nombrecompleto: function() {
+			return this.nombre + ' ' + this.apellido;
+		}
+}
 
-/* 
-Const
-Let
-Var
-*/
+//Se puede crear un nuevo atributo sin haberlo declarado anteriormente
+desarrollador1.departamento = 'Desarrollo Backend';
+
+//Para acceder a los datos (ejemplo: nombre del desarrollador)
+//Notación por punto
+console.log(desarrollador1.nombre)
+
+//Notación por corchete
+
+console.log(desarrollador1['nombre']);
+
+console.log(desarrollador1.nombrecompleto());
